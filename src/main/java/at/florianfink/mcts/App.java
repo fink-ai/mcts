243ca34cd5ea22ed7742036f79f5d7962f4690f1
Florian Fink <florianfink@gmx.at>
2020-11-22 19:31:30 +0100

    initialized maven project

---
+++ src/main/java/at/florianfink/mcts/App.java
@@ -0,0 +1,13 @@
+package at.florianfink.mcts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/at/florianfink/mcts/AppTest.java
@@ -0,0 +1,20 @@
+package at.florianfink.mcts;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
